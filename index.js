bf1f19934cba0f218b4a186c4618bd82ee728d7b
calvince <calokongo@gmail.com>
2022-08-27 21:44:04 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+
+document.addEventListener('DOMContentLoaded',init)
+
+function init() {
+    const pText = document.querySelector('p')
+    pText.textContent = 'This is really cool!'
+    return pText
+
+}
+init(pText)
